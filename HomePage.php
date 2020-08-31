948585325d05c1f6dc912798d74141eb008c98bb
munem <munemsah@gmail.com>
2020-09-01 02:31:25 +0600

    HomePage Added

---
+++ HomePage.php
@@ -0,0 +1,198 @@
+<?php include("includes/header.php")  ?>
+<?php include("includes/nav.php")  ?>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="new-year-new-blood.gif" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="giftblood.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="blood2.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center text-black"> DONATION PROCESS</h2>
+		<h4 class="text-center text-black"> The donation process from the time you arrive center until the time you leave</h4>
+	</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-4 col-md-4 col-12">
+	<div class="card">
+  <img class="card-img-top" src="111.jpg" alt="Card image">
+  		<div class="card-body">
+    		<h4  class="card-title">REGISTRATION</h4>
+    			<p> You need to complete a very simple registration form. Which contains all required contact information to enter in the donation process.</p>
+  			</div>
+		</div>
+	</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+	<div class="card">
+  <img class="card-img-top" src="113.jpeg" alt="Card image">
+  		<div class="card-body">
+    		<h4  class="card-title">SCREENING</h4>
+    			<p> A drop of blood from your finger will take for simple test to ensure that your blood iron levels are proper enough for donation process.</p>
+  		</div>
+	</div>
+</div>
+
+		<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+			  <img class="card-img-top" src="115.jpg" alt="Card image">
+				<div class="card-body">
+					<h4  class="card-title">DONATION</h4>
+    			<p> After ensuring and passed screening test successfully you will be directed to a donor bed for donation. It will take only 6-10 minutes.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center text-danger"> CAMPAIGN GALLERY</h2>
+		<h4 class="text-center text-dark"> Our Prestigious voluntary work on campaigns by the team </h4>
+	</div>
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-lg-4 col-md-4 col-12">
+	<div class="card">
+  <img class="card-img-top" src="image1.jpg" alt="Card image">
+  <a href="#" class="btn btn-warning  text-uppercase ">Dhaka</a>
+		</div>
+		
+	</div>
+
+<div class="col-lg-4 col-md-4 col-12">
+	<div class="card">
+  <img class="card-img-top" src="image5.jpeg" alt="Card image">
+  		<a href="#" class="btn btn-warning text-uppercase ">Chittagong</a>
+	</div>
+	
+</div>
+
+		<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+			  <img class="card-img-top" src="image2.jpeg" alt="Card image">
+				<a href="#" class="btn btn-warning  text-uppercase">Rajshahi</a>
+				</div>
+				
+		</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+			  <img class="card-img-top" src="image11.jpg" alt="Card image">
+				<a href="#" class="btn btn-warning text-uppercase">Sylhet</a>
+				</div>
+				
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+			  <img class="card-img-top" src="image12.jpg" alt="Card image">
+				<a href="#" class="btn btn-warning text-uppercase ">Khulna</a>
+				</div>
+				
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<div class="card">
+			  <img class="card-img-top" src="image13.jpg" alt="Card image">
+				<a href="#" class="btn btn-warning text-uppercase">Rangpur</a>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<section class="container ourservices text-center">
+
+	<div class="row rowsetting">
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-10 d-block m-auto">
+        <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-heart-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+</svg>
+				
+				<h2 class="text-danger"> 2555</h2>
+				<h4> Success Simle</h4>
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-10 d-block m-auto">		
+        <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-droplet-half" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M7.21.8C7.69.295 8 0 8 0c.109.363.234.708.371 1.038.812 1.946 2.073 3.35 3.197 4.6C12.878 7.096 14 8.345 14 10a6 6 0 0 1-12 0C2 6.668 5.58 2.517 7.21.8zm.413 1.021A31.25 31.25 0 0 0 5.794 3.99c-.726.95-1.436 2.008-1.96 3.07C3.304 8.133 3 9.138 3 10c0 0 2.5 1.5 5 .5s5-.5 5-.5c0-1.201-.796-2.157-2.181-3.7l-.03-.032C9.75 5.11 8.5 3.72 7.623 1.82z"/>
+  <path fill-rule="evenodd" d="M4.553 7.776c.82-1.641 1.717-2.753 2.093-3.13l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448z"/>
+</svg>
+				
+				<h2 class="text-danger"> 3555</h2>
+				<h4> Happy Donor</h4>
+
+		</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-10 d-block m-auto">		
+        <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-emoji-smile" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+  <path fill-rule="evenodd" d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683z"/>
+  <path d="M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm4 0c0 .828-.448 1.5-1 1.5s-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5z"/>
+</svg>
+				
+				<h2 class="text-danger"> 1900</h2>
+				<h4> Happy Recipient</h4>
+
+
+	</div>
+
+		<div class="col-lg-3 col-md-3 col-sm-3 col-10 d-block m-auto ">		
+        <svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-award" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M9.669.864L8 0 6.331.864l-1.858.282-.842 1.68-1.337 1.32L2.6 6l-.306 1.854 1.337 1.32.842 1.68 1.858.282L8 12l1.669-.864 1.858-.282.842-1.68 1.337-1.32L13.4 6l.306-1.854-1.337-1.32-.842-1.68L9.669.864zm1.196 1.193l-1.51-.229L8 1.126l-1.355.702-1.51.229-.684 1.365-1.086 1.072L3.614 6l-.25 1.506 1.087 1.072.684 1.365 1.51.229L8 10.874l1.356-.702 1.509-.229.684-1.365 1.086-1.072L12.387 6l.248-1.506-1.086-1.072-.684-1.365z"/>
+  <path d="M4 11.794V16l4-1 4 1v-4.206l-2.018.306L8 13.126 6.018 12.1 4 11.794z"/>
+</svg>
+				
+				<h2 class="text-danger"> 955</h2>
+				<h4> Total Awards</h4>
+
+		</div>	
+	</div>
+<br>
+<br>
+	<div>
+		<h1> Become A Part Of Great Work Today </h1>
+		<p>You can give blood at any of our blood donation venues all over the world.<br>
+We have total sixty thousands donor centers and visit thousands of other venues on various occasions.</p>
+<br>
+	</div>
+	<div> <a href="#" class="btn btn-danger text-center">JOIN WITH US</a></div>
+
+</section>
+<br>
+<br>
+
+<?php include("includes/footer.php")  ?>
