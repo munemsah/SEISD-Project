a2eaf699dc6608c96811ecc7851df3b3164f938b
munem <munemsah@gmail.com>
2020-09-03 01:24:47 +0600

    nav modifited 1

--- Includes/nav.php
+++ Includes/nav.php
@@ -13,10 +13,10 @@
   <div class="collapse navbar-collapse" id="collapsibleNavId">
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link text-danger" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link text-danger" href="HomePage.php">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" href="#">About us</a>
+        <a class="nav-link text-white" href="#">Find Donor</a>
       </li>
       <li class="nav-item">
         <a class="nav-link text-danger" href="#">Donate Blood</a>
@@ -25,23 +25,24 @@
         <a class="nav-link text-white" href="#">Request for blood</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-danger" href="#">Learn</a>
+        <a class="nav-link text-danger" href="#">Campaign</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-white" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <a class="nav-link dropdown-toggle text-white" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
         <div class="dropdown-menu" aria-labelledby="dropdownId">
         <a class="dropdown-item" href="#">Registration as a Volunteer</a>
         <a class="dropdown-item" href="#">Bolod Donation Process</a>
         <a class="dropdown-item" href="#">Blood Types</a>
         <a class="dropdown-item" href="#">Eligibility</a>
         <a class="dropdown-item" href="#">Events</a>
+        <a class="dropdown-item" href="#">FAQ</a>
         </div>
       </li>
       <li class="nav-item">
         <a class="nav-link text-danger" href="#">Blood Bank</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link text-white" href="#">Find Donor</a>
+        <a class="nav-link text-white" href="aboutus.php">About us</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
