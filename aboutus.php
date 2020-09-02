38239004a204c933dc604be7520e3b9275163556
munem <munemsah@gmail.com>
2020-09-03 01:25:32 +0600

    aboutus page modified

--- aboutus.php
+++ aboutus.php
@@ -1,9 +1,16 @@
 <?php include("includes/header.php")  ?>
 <?php include("includes/nav.php")  ?>
 
-<div class="container text-center headerset">
-	<h3 class="text-warning border-danger text-capitalize">About Us</h3>
-	 <p class="text-warning border-danger text-justify">Our website is totally non-commercial non-political, non-communal, non-regional, non-racial, secular and voluntary social organization. Our Goals and objectives are Initiating social movement to build up a healthy society through motivating voluntary blood donation, donating blood voluntarily and others services and awareness programs.</p>
-   </div>
+<div class="card bg-dark text-white">
+  <img src="300.jpg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+  <br>
+  <br>
+  <h3 class="text-warning border-danger text-capitalize d-flex justify-content-center">About Us</h3>
+  <br>
+  <br>
+	 <p class="text-warning border-danger d-flex justify-content-center">Our website is totally non-commercial non-political, non-communal, non-regional, non-racial, secular and voluntary social organization. Our Goals and objectives are Initiating social movement to build up a healthy society through motivating voluntary blood donation, donating blood voluntarily and others services and awareness programs.</p>
+  </div>
+</div>
 
 <?php include("includes/footer.php")  ?>
