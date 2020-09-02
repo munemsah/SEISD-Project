1719e038aae55ce9008b4d8b77908d89d5a5a700
munem <munemsah@gmail.com>
2020-09-03 04:05:45 +0600

    Allcampaign file added

---
+++ Allcampaign.php
@@ -0,0 +1,121 @@
+<?php include("includes/header.php")  ?>
+<?php include("includes/nav.php")  ?>
+
+<div class="jumbotron">
+
+<img src="Image-up\701.jpg" class="card-img-top mx-auto d-block" alt="..." >
+<br>
+
+  <h1 class="display-4">ALL CAMPAIGNS</h1>
+  <br>
+  <p class="lead">Encourage new donors to join and continue to give blood. We have total sixty thousands donor centers and visit thousands of other venues on various occasions.</p>
+  
+<br>
+<br>
+<section class="container ourservices text-center">
+
+  <div class="container row row-cols-1 row-cols-md-2 ">
+  
+
+
+  <div class="card mb-6" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="Image-up\g4.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted">01 February 2020</small></p>
+        <h5 class="card-title">Opening Donation Day</h5>
+        <p class="card-text text-justify">Lorem ipsum dolor sit amet turadipi scing elit lobort issim consecte dign pharetra mauris.
+</p>
+        <p class="card-text"><small class="text-muted">Chittagong, Bangladesh</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+  
+  <div class="card mb-6" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="Image-up\g1.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted">01 March 2020</small></p>
+
+        <h5 class="card-title">Free Group Checking</h5>
+        <p class="card-text text-justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Chittagong, Bangladesh</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+
+<br>
+<br>
+
+
+<div class="container row row-cols-1 row-cols-md-2">
+
+
+  <div class="card mb-6" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="Image-up\g2.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted">01 January 2020</small></p>
+        <h5 class="card-title">Blood Group Collection</h5>
+        <p class="card-text text-justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Chittagong, Bangladesh</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+  
+  <div class="card mb-6" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="Image-up\g3.jpg" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+      <p class="card-text"><small class="text-muted">01 August 2020</small></p>
+        <h5 class="card-title">Blood Donation Camp</h5>
+        <p class="card-text text-justify">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Chittagong, Bnagladesh</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
+<br>
+<br>
+
+<div>
+		<h1> Become A Part Of Great Work Today </h1>
+        <br>
+		<p>You can give blood at any of our blood donation venues all over the world.<br>
+We have total sixty thousands donor centers and visit thousands of other venues on various occasions.</p>
+<br>
+	</div>
+	<div> <a href="#" class="btn btn-danger text-center">JOIN WITH US</a></div>
+</selection>
+</div>
+
+<?php include("includes/footer.php")  ?>
