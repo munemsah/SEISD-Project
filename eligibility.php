659ed3b695a7f0409416d9c1d1fbe568ff2340fd
munem <munemsah@gmail.com>
2020-09-03 02:09:46 +0600

    add bloodtype, eligibility pages

---
+++ eligibility.php
@@ -0,0 +1,52 @@
+<?php include("includes/header.php")  ?>
+<?php include("includes/nav.php")  ?>
+<br>
+  <br>
+<div class="container">
+<div class="card mb-3">
+  <img src="Image-up\eligibility.jpg" class="card-img-top mx-auto d-block" alt="..." style="max-width: 700px;">
+  <div class="card-body justify-content-center">
+ 
+
+<h2 style="color:Red;" class="text-justify d-flex justify-content-center">  &nbsp; General Blood & Platelet Donor Guidelines</h2>
+
+<br>
+<h3 style="color:blue;">&nbsp; Why Guidelines Are Important</h3>
+<p class="text-justify"> Donor eligibility rules help to protect the health and safety of the donor as well as the person who will receive a blood transfusion. The general guidelines listed below will help you determine if you are eligible to donate blood or platelets.</p>
+
+<h3 style="color:blue;"> &nbsp; You and Your Donation Are Important to Us</h3>
+<p class="text-justify"> Before donating, one of our medical professionals will discuss your health history with you in a private, confidential setting. After taking your pulse, blood pressure, and temperature and checking for anemia, we will determine whether you are eligible to be a donor.</p>
+
+<h3 style="color:blue;">&nbsp; General Guidelines
+</h2>
+<p class="text-justify">To donate blood or platelets, you must be in good general health, weigh at least 110 pounds, and be at least 16 years old. Parental consent is required for blood donation by 16 year olds; 16 year olds are NOT eligible to donate platelets. No parental consent is required for those who are at least 17 years old. If you are 76 or older, you will need your doctor’s written approval for blood or platelet donation.</p>
+
+<h3 style="color:blue;">&nbsp; What Is ‘Good’ Health?</h3>
+<p class="text-justify">Good health means that you feel well and are able to carry out normal daily activities. If you have a chronic medical condition such as diabetes or high blood pressure, you may still be eligible as long as you are receiving treatment to control your condition.</p>
+
+<h3 style="color:blue;">&nbsp; Needed Identification</h3>
+<p>We require that you provide identification that shows your name and your photograph or signature.</p>
+
+<h3 style="color:blue;">&nbsp; What Conditions Would Make You Ineligible to Be a Donor?</h3>
+<p class="text-justify">
+***  Have tested positive for hepatitis B or hepatitis C, lived with or had sexual contact in the past 12 months with anyone who has hepatitis B or symptomatic hepatitis C. <br>
+***  Had a tattoo in the past 12 months or received a blood transfusion (except with your own blood) in the past 12 months. <br>
+***  Have ever had a positive test for the AIDS virus. <br>
+***  Are a man who has had sex with another man in the past 12 months.<br>
+***  Have ever used intravenous illegal drugs, even once.<br>
+***  Have ever used injectable drugs, including anabolic steroids, unless prescribed by a physician.<br>
+***  Have engaged in prostitution since 1977.<br>
+***  Have lived in or visited the United Kingdom for three months or more cumulatively between 1980 and 1996.<br>
+***  Have spent five years or more in Europe (including the United Kingdom) cumulatively from 1980 to the present.<br>
+***  Have traveled in the past year, or lived in the past three years, in an area where malaria is endemic.</p>
+
+
+
+<h3 style="color:blue;">&nbsp; Additional Criteria</h3>
+<p class="text-justify">
+It is important that you understand the criteria that permit you to donate blood or platelets. In addition to the general requirements listed above, please take the time to review Additional Donor Requirements.</p>
+
+  </div>
+</div>
+</div>
+<?php include("includes/footer.php")  ?>
