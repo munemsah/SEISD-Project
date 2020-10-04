436a7a035af71572b5aef56ae13541dd4a17d068
istiyak33 <70500922+istiyak33@users.noreply.github.com>
2020-10-04 23:31:26 +0600

    Add files via upload

---
+++ Deshboard.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<title>Blood Bank</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<body>
+
+<!-- Sidebar -->
+<div class="w3-sidebar  w3-black w3-bar-block" style="width:15%">
+  <h4 class="w3-bar-item" >Dashboard</h4>
+  <a href="#" class="w3-bar-item w3-button">Home</a>
+  <a href="#" class="w3-bar-item w3-button">Donor list</a>
+  <a href="#" class="w3-bar-item w3-button">Blood Request</a>
+  <a href="#" class="w3-bar-item w3-button">Blog Post</a>
+  <a href="#" class="w3-bar-item w3-button">Campaign Galery</a>
+  <a href="#" class="w3-bar-item w3-button ">Profile</a>
+  <a href="#" class="w3-bar-item w3-button">Tools</a>
+  <a href="#" class="w3-bar-item w3-button">Settings</a>
+
+</div>
+
+<!-- Page Content -->
+<div style="margin-left:15%">
+
+    <div class="w3-container w3-black">
+                <h4 class="text-right">
+                     <div class="dropdown">
+                        <button class="btn btn-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Profile </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                               <a class="dropdown-item" href="#">Profile</a>
+                                  <a class="dropdown-item" href="#">Setting</a>
+                                 <a class="dropdown-item" href="#">Log out</a>
+                       </div>
+                     </div>
+                 </h4>
+    </div>
+
+
+
+
+
+
+
+</div>
+      
+</body>
+</html>
